71f0e200e887d1a171f65546b99c88ee8de59d9e
Ömer Büyükgüllü <omerbg.is01@gmail.com>
2023-08-19 21:14:10 +0300

    v1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,23 @@
+public class Class01 {
+    public static void main(String[] args) {
+
+        // versiyon 1 kodları
+        System.out.println("versiyon 1");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
